00df0bb1e7a488e498492a9a599721178f29ffab
hiraPince <LI56211100883284@163.com>
2021-03-13 18:18:47 +0800

    重新服务端，添加多人聊天功能

--- UDP-client2.py
+++ UDP-client2.py
@@ -1,15 +1,18 @@
+# -*- coding: utf-8 -*-
 import socket
 from tkinter import *
 import time, threading
 import tkinter.messagebox as messagebox
-stop=0
+#HOST ='172.93.34.44'
+HOST = '172.28.32.140'
+PORT = 9999
+ADDR = (HOST, PORT)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 root = Tk()
 root.title("Hi chat!")
 root.geometry('300x200')
-
-
 
 
 sendvar = StringVar()
@@ -22,33 +25,36 @@ receive = Entry(root, textvariable = receivevar)
 receivevar.set("receive message!")
 receive.pack()
 
-def sendmessage():
+def send():
     # 发送数据:
     #s.sendto(data, ('172.93.34.44', 9999))
     print('send data is: %s' %sendvar.get())
-    s.sendto(sendvar.get().encode('utf-8'), ('172.28.32.140', 9999))
+    s.sendto(sendvar.get().encode('utf-8'), ADDR)
     #s.sendto(sendvar.get().encode('utf-8'), ('172.93.34.44', 9999))
-Button(root, text="send", command = sendmessage).pack()
+Button(root, text="send", command = send).pack()
 
-def loop():
-    global stop
+def exit():
+    s.sendto('quit'.encode('utf-8'),ADDR)
+    ExitButton['text']='exit ok'
+    root.destroy()
+
+ExitButton=Button(root, text="exit", command = exit)
+ExitButton.pack()
+def recv_loop():
     global s
     print('thread %s is running...' % threading.current_thread().name)
     while True:
-        time.sleep(1)
+        #time.sleep(3)
         # 接收数据:
         rec=s.recv(1024).decode('utf-8')
         print('接收的数据是：%s'%rec )
+        if rec == 'quit':
+            break
         receivevar.set(rec)
-    return
+
 s.sendto('hello'.encode('utf-8'), ('172.28.32.140', 9999))
 print('thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target=loop, name='LoopThread')
+t = threading.Thread(target=recv_loop, name='LoopThread')
 t.start()
-#t.join()
-
 root.mainloop()
-#t.join(2)
-t.stop()
-time.sleep(6)
 s.close()
--- UDP-server.py
+++ UDP-server.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import socket
 global a
 global b
 a=''
 b=''
+clients = {}
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # 绑定端口:
 #s.bind(('172.93.34.44', 9999))
@@ -15,55 +17,35 @@ while True:
 
     data, addr = s.recvfrom(1024)
     print('Received from %s:%s.' % addr)
-    print('data[0:3]:%s '%data[:3])
-    if data[:3] == b'aaa':
-        length=len(data)
-        a += data[3:length].decode('utf-8')
-        addrb = addr
-        print('Received a len: %d is: %s '%(length ,a))
-        # if len(b)!=0:
-        #     s.sendto(b'Hello, %s' % b.encode('utf-8'), addr)
-        #     b=''
-        # else:
-        #     s.sendto(b'Hello, %s!' % 'No message'.encode('utf-8'), addr)
 
-    if data[:3] == b'bbb':
-        length=len(data)
-        b += data[3:length].decode('utf-8')
-        addra = addr
-        print('Received b len: %d is: %s '%(length ,b))
-        # if len(a)!=0:
-        #     s.sendto(b'Hello, %s' % a.encode('utf-8'), addr)
-        #     a=''
-        # else:
-        #     s.sendto(b'Hello, %s!' % 'No message'.encode('utf-8'), addr)
-    #s.sendto(b'Hello, %s!' % data, addr)
-    # if len(a) != 0:
-    #
-    #     if addra != (0,0):
-    #         s.sendto(b'Hello, %s' % a.encode('utf-8'), addra)
-    #         a = ''
-    # else:
-    #     if addra!=(0,0):
-    #         s.sendto(b'Hello, %s!' % 'No message'.encode('utf-8'), addra)
-    #
-    # if len(b) != 0:
-    #     if addrb != (0,0):
-    #         s.sendto(b'Hello, %s' % b.encode('utf-8'), addrb)
-    #         b = ''
-    # else:
-    #     if addrb != (0,0):
-    #         s.sendto(b'Hello, %s!' % 'No message'.encode('utf-8'), addrb)
-    print('addra is: %s:%s ' % addra)
-    print('addrb is: %s:%s ' % addrb)
-    if len(a) != 0:
-        if addra != (0, 0):
-            print('aaaaaaaaaa')
-            s.sendto(b'Hello, %s' % a.encode('utf-8'), addra)
-            a = ''
+    clientA=data[:3]
+    clientB=data[5:8]
+    print('clientA:%s  clientB:%s' % (data[:3],data[5:8]))
+    if data[8:12] == b'quit':
+        s.sendto(data[8:12], addr)
+        del clients[clientA]
+    elif clients.has_key(clientA):
+        print('1111111111')
+        print(' %s: is exist '%clientA + ', addr is:  %s:%s' %clients[clientA][0] )
+        if clients.has_key(clientB):
+            print('--11--clientB  exist !')
+            print('send to name is %s '%clientB + ',addr is:  %s:%s.'%clients[clientB][0])
+            s.sendto(b'%s' % data[8:], clients[clientB][0])
+        else:
+            print('--11--clientB not exist !')
+            s.sendto(b'%s not online!' % clientB, clients[clientA][0])
+    elif  not clients.has_key(clientA):
+        print('22222222')
+        print(' %s is not exist ' % clientA)
+        s.sendto(b'welcome %s' %clientA , addr)
+        clients[clientA]=[addr,data]
+        #clients[clientA][0]= addr
+        #clients[clientA][1]= data
+        if clients.has_key(clientB):
+            print('--22--clientB  exist !')
+            s.sendto(b'%s' % data[8:], clients[clientB][0])
+        else:
+            print('--22--clientB not exist !')
+            #print('welcome:%s ' % clientB)
+            s.sendto(b'%s not online!' % clientB, clients[clientA][0])
 
-    elif len(b) != 0:
-        if addrb != (0, 0):
-            print('bbbbbbbbbbb')
-            s.sendto(b'Hello, %s' % b.encode('utf-8'), addrb)
-            b = ''
