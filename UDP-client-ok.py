2fc46d31586a7aa7b79d9ae1ab300a1a24630dc1
hiraPince <LI56211100883284@163.com>
2021-03-16 18:32:26 +0800

    更好的中文支持，友好的UI界面，规范的数据包格式

---
+++ UDP-client-ok.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import socket
+from tkinter import *
+import time, threading
+#import tkinter.messagebox as messagebox
+import re
+
+class chat:
+    #HOST = '172.28.32.140'
+    HOST ='172.93.34.44'
+    PORT = 9999
+    ADDR = (HOST, PORT)
+
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.root = Tk()
+        self.root.title("Hi Chat")
+        self.root.geometry('380x470')
+        self.frm = Frame(self.root)
+
+        #top
+        self.frm_T = Frame(self.frm)
+
+        #Entry for Master name
+        self.master = StringVar()
+        self.MasterEntry = Entry(self.frm_T, textvariable = self.master,width=8,font=('Verdana', 15))
+        self.master.set("bbb")
+        self.MasterEntry.pack(side=LEFT)
+
+        # to
+        Label(self.frm_T, text='to', width=8,font=('Arial', 12)).pack(side=LEFT)
+
+        #client name
+        self.client = StringVar()
+        self.ClientEntry = Entry(self.frm_T, textvariable = self.client,width=8, font=('Verdana', 15))
+        self.client.set("aaa")
+        self.ClientEntry.pack(side=RIGHT)
+        self.frm_T.pack()
+
+        #middle
+        self.frm_M = Frame(self.frm)
+        self.t_show = Text(self.frm_M, width=20, height=15, font=('Verdana', 15))
+        self.t_show.insert('1.0', '')
+        self.t_show.pack(fill=BOTH)
+        self.chat = StringVar()
+        self.ChatEntry = Entry(self.frm_M, textvariable = self.chat,width=8, font=('Verdana', 15))
+        self.chat.set("Enter message")
+        self.ChatEntry.pack(fill=BOTH)
+        self.frm_M.pack()
+
+        #bottom
+        self.frm_MB = Frame(self.frm)
+        self.SendButton=Button(self.frm_MB, text="发送",width=14, command=self.send).pack(side=LEFT)
+        self.ExitButton=Button(self.frm_MB, text="退出", command=self.exit).pack(side=RIGHT)
+        self.frm_MB.pack()
+        self.frm.pack()
+
+        #self.s.sendto('hello'.encode('utf-8'), ('172.28.32.140', 9999))
+        self.s.sendto('hello'.encode('utf-8'), self.ADDR)
+        print('thread %s is running...' % threading.current_thread().name)
+        self.t = threading.Thread(target=self.recv_loop, name='LoopThread')
+        self.t.start()
+        #self.root.mainloop()
+
+
+    # def send(self):
+    #     print('test!')
+
+    def send(self):
+        # 发送数据:
+        # s.sendto(data, ('172.93.34.44', 9999))
+        if self.master.get()=='':
+            self.t_show.insert(END,'请输入您的姓名！\n')
+            return
+
+        if self.client.get() == '':
+            self.t_show.insert(END, '请输入对方姓名！\n')
+            return
+
+        tmpdata=b'<'+self.master.get().encode('utf-8')+b' to '+self.client.get().encode('utf-8')+b'>data='+self.chat.get().encode('utf-8')
+        print('send data is: %s'% tmpdata)
+        self.s.sendto(tmpdata, self.ADDR)
+        # s.sendto(sendvar.get().encode('utf-8'), ('172.93.34.44', 9999))
+        self.t_show.insert(END, '  ' + self.chat.get() + ':' + self.master.get() + '\n')
+        self.chat.set('')
+
+    def exit(self):
+        self.s.sendto(b'<'+self.master.get().encode('utf-8')+b' to '+self.client.get().encode('utf-8')+b'><!quit>', self.ADDR)
+        #self.s.sendto(b'<'+self.master.get().encode('utf-8')+b' to '+self.client.get().encode('utf-8')+b'><!quit>data=',self.ADDR)
+        #self.s.sendto( '<!quit>data='.encode('utf-8'),self.ADDR)
+
+        #self.ExitButton['text'] = '成功'
+        time.sleep(1)  #一定要延时，否则没等消息过来终止线程，窗口退出会异常
+        self.root.destroy()
+
+    def recv_loop(self):
+        print('thread %s is running...' % threading.current_thread().name)
+        while True:
+            # time.sleep(3)
+            # 接收数据:
+            #self.rec = self.s.recv(1024).decode('utf-8')
+            self.rec = self.s.recv(1024)
+            print('接收的数据是：%s' % self.rec.decode('utf-8'))
+            #patternclient =   # 匹配查找client 名字8
+            if re.search(re.compile(b"<!quit>"),self.rec) !=None:
+                break
+            if re.search(re.compile(b"(?<=<).+?(?= to)"), self.rec)!=None:
+                clientname = re.search(re.compile(b"(?<=<).+?(?= to)"), self.rec).group(0).decode('utf-8')
+                if re.search(re.compile(b"(?<=data=).+"),self.rec)!=None:
+                    self.t_show.insert(END,clientname+':'+re.search(re.compile(b"(?<=data=).+"),self.rec).group(0).decode('utf-8')+'\n')
+                    if self.master.get()!=clientname:   #防止自己跟自己聊天
+                        self.client.set(clientname)
+
+
+
+
+def main():
+    d = chat()
+    mainloop()
+    d.s.close()
+
+
+if __name__ == "__main__":
+    main()
--- UDP-server.py
+++ UDP-server.py
@@ -8,8 +8,8 @@ b=''
 clients = {}
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # 绑定端口:
-#s.bind(('172.93.34.44', 9999))
-s.bind(('172.28.32.140', 9999))
+s.bind(('172.93.34.44', 9999))
+#s.bind(('172.28.32.140', 9999))
 print('Bind UDP on 9999...')
 addra = (0, 0)
 addrb = (0, 0)
@@ -18,60 +18,64 @@ while True:
 
     data, addr = s.recvfrom(1024)
     print('connect from %s:%s.' % addr)
-
-
-    print('data is:     %s.' % data)
-    if data == b'hello':
+    print('data is:*************  %s.' % data)
+    if data == b'hello':               #登陆时候欢迎
         print('--------- from %s:%s.' % addr)
         continue
-    patternA = re.compile(b"(?<=<).+?(?= to)")#匹配查找clientA 名字
-    clientA=re.search(patternA, data).group(0)
 
-    patternB = re.compile(b"(?<=to ).+?(?=>)") #匹配查找clientB 名字
-    clientB = re.search(patternB, data).group(0)
+
+    if re.search(re.compile(b"(?<=<).+?(?= to)"), data)!=None:  #匹配查找clientA 名字,如果为空则默认名字master
+        clientA=re.search(re.compile(b"(?<=<).+?(?= to)"), data).group(0)
+    else:
+        clientA=b'master'
+
+    if re.search(re.compile(b"(?<=to ).+?(?=>)"), data)!=None: #匹配查找clientB 名字,如果为空则默认名字client
+        clientB = re.search(re.compile(b"(?<=to ).+?(?=>)"), data).group(0)
+    else:
+        clientA = b'client'
+
     print('clientA:%s  clientB:%s' % (clientA,clientB))
+    #header=b'<'+clientA+' to '+clientB+'>'
 
-    patternheader=re.compile(b"(?<=<).+?(?=>)")
-    if re.search(patternheader, data)!=None:
-         header=re.search(patternheader, data).group(0)
-
-    pattern_quit = re.compile(b"<!quit>")
-    patterndata = re.compile(b"(?<=data=).+")   #匹配是否有有效数据
-
-    if re.search(pattern_quit,data)!=None: #如果匹配到有quit,就执行退出操作并删除用户
+    if re.search(re.compile(b"<!quit>"),data)!=None: #如果匹配到有quit,就执行退出操作并删除用户
         s.sendto(data, addr)
         if clients.has_key(clientA):
             del clients[clientA]
             print('delete %s'% clientA)
 
-
-    elif re.search(patterndata,data)==None:
+    if re.search(re.compile(b"(?<=data=).+"),data)==None:  #如果发过来的数据是空，则不做操作
         continue
 
+    if re.search(re.compile(b"<.+?>"), data)!=None:   #获取数据包头信息
+          header=re.search(re.compile(b"<.+?>"), data).group(0)
 
-    elif clients.has_key(clientA):
+
+    if clients.has_key(clientA):   #clientA 存在的情况
         print('1111111111')
         print(' %s: is exist '%clientA + ', addr is:  %s:%s' %clients[clientA][0] )
-        if clients.has_key(clientB):
-            print('--11--clientB  exist !')
+        if clients.has_key(clientB):    #clientB 存在的情况
+            print('--11--clientB %s exist !'%clientB)
             print('send to name is %s '%clientB + ',addr is:  %s:%s.'%clients[clientB][0])
+            #s.sendto(header + b'data=' + b'%s 在线' % clientB, clients[clientA][0])
             s.sendto(b'%s' % data, clients[clientB][0])
-        else:
-            print('--11--clientB not exist !')
-            s.sendto(b'%s not online!' % clientB, clients[clientA][0])
+
+        else:        #clientB 不存在的情况
+            print('--11--clientB  %s not exist !' %clientB)
+            s.sendto(header+b'data='+b'%s 不在线' % clientB, clients[clientA][0])
 
 
-    elif  not clients.has_key(clientA):
+    elif  not clients.has_key(clientA):    #clientA 不存在的情况
 
-        print('22222222')
-        print(' %s is not exist ' % clientA)
-        s.sendto(header+b'welcome %s' %clientA , addr)
+        print('22222222 header is %s'% header)
+        print(' %s is not exist' % clientA)
+        s.sendto(header+b'data='+b'welcome %s' % clientA , addr)
         clients[clientA]=[addr,data]
-        if clients.has_key(clientB):
-            print('--22--clientB  exist !')
+        if clients.has_key(clientB):    #clientB 存在的情况
+            print('--22--clientB %s exist !' %clientB)
+            #s.sendto(header + b'data=' + b'%s 在线' % clientB, clients[clientA][0])
             s.sendto(b'%s' % data, clients[clientB][0])
-        else:
+        else:                  #clientB 不存在的情况
             print('--22--clientB not exist !')
             #print('welcome:%s ' % clientB)
-            s.sendto(header+b'%s not online!' % clientB, clients[clientA][0])
+            s.sendto(header+b'data='+b'%s 不在线' % clientB, clients[clientA][0])
 
